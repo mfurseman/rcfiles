bc51d1154d831d3f33037b65a89ef68bbf743513
Matthew Furseman <matthew.furseman@ukaea.uk>
2021-09-11 09:20:38 +0100

    Don't use date order by default in git log

--- .config/fish/functions/gl.fish
+++ .config/fish/functions/gl.fish
@@ -1,4 +1,4 @@
 # Defined in - @ line 0
 function gl --description 'alias gl git log --graph --abbrev-commit --pretty=oneline --decorate --date-order --all'
-	git log --graph --abbrev-commit --pretty=oneline --decorate --date-order --all $argv;
+	git log --graph --abbrev-commit --pretty=oneline --decorate --all $argv;
 end
