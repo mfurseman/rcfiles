c9c630fca3cfcf06037a32ea349f349d7e06a7fc
Matthew Furseman <matthew.furseman@ukaea.uk>
2022-06-13 14:57:32 +0100

    Add logic to check if we are on Win11 or have to fallback to VcxSrv

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,10 @@
+export WIN_VER=(echo (cmd.exe /c ver) | sed 's/.*\[Version \([^]]*\)\].*/\1/g')
 export WSL_HOST=(tail -1 /etc/resolv.conf | cut -d' ' -f2)
-export DISPLAY=$WSL_HOST:0.0
+export DISPLAY=:0
+# Fall back to vcxsrv for Windows 10
+if test (echo $WIN_VER | cut -d "." -f 3) -lt 22000
+    export DISPLAY=$WSL_HOST:0.0
+end
 export LIBGL_ALWAYS_INDIRECT=1
 set PATH /mnt/c/Windows/ $PATH
 set PATH /mnt/c/Windows/System32 $PATH
