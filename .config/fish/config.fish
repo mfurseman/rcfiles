5a37c67719c916ccd175046eac71519a859af6c1
Matthew Furseman <matthew.furseman@ukaea.uk>
2022-10-12 16:02:16 +0100

    Don't attempt to use Windows binaries if they arn't available

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,8 @@
-export WIN_VER=(powershell.exe  [Environment]::OSVersion.Version.Build)
+if which powershell.exe > /dev/null
+    export WIN_VER=(powershell.exe  [Environment]::OSVersion.Version.Build)
+    set PATH (dirname (which cmd.exe)) $PATH
+    set PATH (dirname (dirname (which cmd.exe))) $PATH
+end
 export WSL_HOST=(tail -1 /etc/resolv.conf | cut -d' ' -f2)
 export DISPLAY=:0
 # Fall back to vcxsrv for Windows 10
@@ -6,7 +10,5 @@ if test (echo $WIN_VER | cut -d "." -f 3) -lt 22000
     export DISPLAY=$WSL_HOST:0.0
 end
 export LIBGL_ALWAYS_INDIRECT=1
-set PATH /mnt/c/Windows/ $PATH
-set PATH /mnt/c/Windows/System32 $PATH
 set_path $HOME/.local
 bass source theme_sync
